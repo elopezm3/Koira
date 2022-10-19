f42650121c3faa1bb7c5695244b5741668bd034b
Wilber Tuberquia Madrid <wilbertuberquia234621@correo.itm.edu.co>
2022-10-18 21:23:55 -0500

    Delete userController and fix sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   # end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :permission_level])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :permission_level])
   end
 
   private
