bbeb544716ce5decd4d85638aa2b392f91430f68
Wilber Tuberquia Madrid <wilbertuberquia234621@correo.itm.edu.co>
2022-10-20 21:19:16 -0500

    show, new, parcial_create,destroy and update

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   get "/pages/supplier", to: "pages#supplier", as: "supplier"
   resources :businesses, only: [:index, :update, :show, :destroy]
   get "/user_id/businesses/new", to: "businesses#new", as: "new_user_business"
-  post "/users/:user_id/businesses", to: "businesses#create", as: "create_user_business"
+  #post "/users/:user_id/businesses", to: "businesses#create", as: "create_user_business"
+  get "/users/:user_id/businesses", to: "businesses#create", as: "create_user_business"
   get "/users/:user_id/user_interests/new", to: "user_interests#new", as: "new_user_interest"
   post "/users/:user_id/user_interests", to: "user_interests#create", as: "create_user_interest"
   resources :purchases, only: [:show]
   get "/users/:user_id/purchases/new", to: "user_interests#new", as: "new_user_purchase"
   post "/users/:user_id/purchases", to: "user_interests#create", as: "create_user_purchase"
   get "/my_business/:business_id", to: "business#show_for_owner", as: "my_business"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
