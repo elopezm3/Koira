ca052f311f510c3a98adc3bf09a80979da6debd1
Wilber Tuberquia Madrid <wilbertuberquia234621@correo.itm.edu.co>
2022-10-29 16:52:22 -0500

    Add form edit, edit in userShow and edit product or services Controller

--- app/controllers/product_or_services_controller.rb
+++ app/controllers/product_or_services_controller.rb
@@ -4,6 +4,18 @@ class ProductOrServicesController < ApplicationController
     @product_or_service = ProductOrService.new
   end
 
+  def edit
+
+  end
+
+  def update
+    if @product_or_service.update(product_or_service_params)
+      redirect_to product_or_service_path(@product_or_service)
+    else
+      render :edit
+    end
+  end
+
   def create
     @business = Business.find(params[:business_id])
     @product_or_service = ProductOrService.new(product_or_service_params)
