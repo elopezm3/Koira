42aaabdda8235f8b38e2e7b290dcb75ceada8d6b
Esteban Lopez <estebanlopez_99@hotmail.com>
2022-11-02 16:23:02 -0500

    [ADD] dependent destroy to models

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,4 +1,4 @@
 class Interest < ApplicationRecord
-  has_many :user_interests
+  has_many :user_interests, dependent: :destroy
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :user_interests
-  has_many :my_purchases, class_name: "Purchase", foreign_key: "buyer_id"
-  has_many :my_businesses, class_name: "Business", foreign_key: "owner_id"
-  has_many :reviews
+  has_many :user_interests, dependent: :destroy
+  has_many :my_purchases, class_name: "Purchase", foreign_key: "buyer_id", dependent: :destroy
+  has_many :my_businesses, class_name: "Business", foreign_key: "owner_id", dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true, inclusion: { in: ["admin", "seller", "customer"] }
